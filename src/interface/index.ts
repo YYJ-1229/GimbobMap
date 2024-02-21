b01a9ecc026100ff602e55e036bfc96819d4372d
YYJ-1229 <90436@naver.com>
2024-02-21 17:14:19 +0900

    [update] : 파일 셋팅

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,10 +1,29 @@
 export interface StoreType {
-  tel_no: string;
-  cob_code_nm: string;
-  bizcnd_code_nm: string;
-  upso_nm: string;
-  x_cnts: string;
-  y_dnts: string;
-  rdn_code_nm: string;
-  crtfc_gbn_nm: string;
+  id: number;
+  phone?: string | null;
+  address?: string | null;
+  lat?: string | null;
+  lng?: string | null;
+  name?: string | null;
+  category?: string | null;
+  storeType?: string | null;
+  foodCertifyName?: string | null;
+}
+
+export interface StoreApiResponse {
+  data: StoreType[];
+  totalPage?: number;
+  totalCount?: number;
+  page?: number;
+}
+
+export interface LocationType {
+  lat?: string | null;
+  lng?: string | null;
+  zoom: number;
+}
+
+export interface SearchType {
+  q?: string | null;
+  district?: string | null;
 }
