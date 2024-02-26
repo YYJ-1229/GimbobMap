3c867c888e6b0a527219cb633c5e5cef178e644d
YYJ-1229 <90436@naver.com>
2024-02-26 17:14:11 +0900

    [update] : like interface 추가 및 생성

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -8,8 +8,14 @@ export interface StoreType {
   category?: string | null;
   storeType?: string | null;
   foodCertifyName?: string | null;
+  likes?: LikeInterface[];
 }
 
+export interface LikeInterface {
+  id: number;
+  storeId: number;
+  userId: number;
+}
 export interface StoreApiResponse {
   data: StoreType[];
   totalPage?: number;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,18 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/db";
+
 import GoogleProvider from "next-auth/providers/google";
 import NaverProvider from "next-auth/providers/naver";
 import KakaoProvider from "next-auth/providers/kakao";
 
-export const authOptions = {
-  adapter: PrismaAdapter(prisma),
+export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt" as const,
     maxAge: 3 * 24 * 60 * 60, //3day
     updateAge: 24 * 60 * 60 //24hours
   },
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
@@ -26,9 +27,26 @@ export const authOptions = {
       clientSecret: process.env.KAKAO_CLIENT_SECRET || ""
     })
   ],
+
   pages: {
     signIn: "/users/login"
-  }
+  },
+  callbacks: {
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.sub
+      }
+    }),
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    }
+  },
+  adapter: PrismaAdapter(prisma)
 };
 
 export default NextAuth(authOptions);
